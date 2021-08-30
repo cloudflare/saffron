520d8e10a82a41907a454bc8969bfac9b777fee2
Aaron Loyd <aloyd@cloudflare.com>
2021-08-30 14:31:20 -0500

    Fix benchmark crons

--- saffron/benches/cron.rs
+++ saffron/benches/cron.rs
@@ -2,7 +2,7 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
 fn cron_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("Cron.from_str");
-    let inputs = ["* * * * *", "1 12 3 6 ?", "12-35 1-23 2-5 1-11 ?"];
+    let inputs = ["* * * * *", "1 12 3 6 *", "12-35 1-23 2-5 1-11 *"];
     for input in inputs.iter() {
         group.bench_with_input(BenchmarkId::from_parameter(input), input, |b, input| {
             b.iter(|| input.parse::<saffron::Cron>().unwrap())
